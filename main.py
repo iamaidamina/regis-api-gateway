4c912043c0795e71b881937f18a26843435a6c73
Aida Mina <iamaidamina@gmail.com>
2022-11-09 22:56:00 -0500

    Creating user login with JWT

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ import re
 
 app=Flask(__name__)
 cors = CORS(app)
+from flask_jwt_extended import create_access_token, verify_jwt_in_request
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+app.config["JWT_SECRET_KEY"]="super-secret" #Cambiar por el que se conveniente
+jwt = JWTManager(app)
+@app.route("/login", methods=["POST"])
+def create_token():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url=dataConfig["url-backend-security"]+'/usuarios/validar'
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        user = response.json()
+        expires = datetime.timedelta(seconds=60 * 60*24)
+        access_token = create_access_token(identity=user, expires_delta=expires)
+        return jsonify({"token": access_token, "user_id": user["_Id"]})
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
 
 @app.route("/",methods=['GET'])
 def test():
@@ -22,6 +41,5 @@ def loadFileConfig():
     return data
 if __name__=='__main__':
     dataConfig = loadFileConfig()
-    print("Server running : "+"http://"+dataConfig["url-backend"]+":" +
-str(dataConfig["port"]))
+    print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
     serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
